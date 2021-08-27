ef3786823196f7293ec24d7305cce0b6d81f9eda
minusInfinite <ashley@minusinfinite.net>
2021-08-27 12:05:39 +0930

    project init

---
+++ db/role.sql
@@ -0,0 +1,63 @@
+insert into role (title, salary, department_id) values'Design Engineer'560001
+insert into role (title, salary, department_id) values'Senior Quality Engineer'700001
+insert into role (title, salary, department_id) values'Structural Engineer'530001
+insert into role (title, salary, department_id) values'VP Product Management'1250001
+insert into role (title, salary, department_id) values'Editor'950002
+insert into role (title, salary, department_id) values'Marketing Assistant'550002
+insert into role (title, salary, department_id) values'Marketing Manager'510002
+insert into role (title, salary, department_id) values'Media Manager'770002
+insert into role (title, salary, department_id) values'VP Marketing'910002
+insert into role (title, salary, department_id) values'Account Coordinator'600003
+insert into role (title, salary, department_id) values'Account Executive'870003
+insert into role (title, salary, department_id) values'Account Representative'550003
+insert into role (title, salary, department_id) values'Director of Sales'1470003
+insert into role (title, salary, department_id) values'Safety Technician'490003
+insert into role (title, salary, department_id) values'Senior Sales Associate'600003
+insert into role (title, salary, department_id) values'VP Sales'950003
+insert into role (title, salary, department_id) values'Administrative Assistant'840004
+insert into role (title, salary, department_id) values'Administrative Officer'360004
+insert into role (title, salary, department_id) values'Community Outreach Specialist'390004
+insert into role (title, salary, department_id) values'General Manager'530004
+insert into role (title, salary, department_id) values'Librarian'560004
+insert into role (title, salary, department_id) values'VP Product Management'1250004
+insert into role (title, salary, department_id) values'Analog Circuit Design manager'850005
+insert into role (title, salary, department_id) values'Associate Professor'950005
+insert into role (title, salary, department_id) values'Chemical Engineer'410005
+insert into role (title, salary, department_id) values'Electrical Engineer'920005
+insert into role (title, salary, department_id) values'Environmental Tech'920005
+insert into role (title, salary, department_id) values'Geologist'290005
+insert into role (title, salary, department_id) values'Mechanical Systems Engineer'890005
+insert into role (title, salary, department_id) values'Product Engineer'260005
+insert into role (title, salary, department_id) values'Programmer'460005
+insert into role (title, salary, department_id) values'Sales Associate'660005
+insert into role (title, salary, department_id) values'Senior Cost Accountant'750005
+insert into role (title, salary, department_id) values'Senior Developer'980005
+insert into role (title, salary, department_id) values'Software Test Engineer'630005
+insert into role (title, salary, department_id) values'Staff Scientist'460005
+insert into role (title, salary, department_id) values'Computer Systems Analyst'800006
+insert into role (title, salary, department_id) values'Desktop Support Technician'580006
+insert into role (title, salary, department_id) values'Help Desk Operator'340006
+insert into role (title, salary, department_id) values'Help Desk Technician'430006
+insert into role (title, salary, department_id) values'Information Systems Manager'490006
+insert into role (title, salary, department_id) values'Quality Control Specialist'660006
+insert into role (title, salary, department_id) values'Quality Engineer'820006
+insert into role (title, salary, department_id) values'Systems Administrator'850006
+insert into role (title, salary, department_id) values'Internal Auditor'560007
+insert into role (title, salary, department_id) values'Legal Assistant'580007
+insert into role (title, salary, department_id) values'Paralegal'890007
+insert into role (title, salary, department_id) values'Automation Specialist'800008
+insert into role (title, salary, department_id) values'Developer'310008
+insert into role (title, salary, department_id) values'Cost Accountant'630009
+insert into role (title, salary, department_id) values'Senior Financial Analyst'520009
+insert into role (title, salary, department_id) values'Assistant Media Planner'6000010
+insert into role (title, salary, department_id) values'Trainer'5300010
+insert into role (title, salary, department_id) values'Trainer'5300010
+insert into role (title, salary, department_id) values'VP Marketing'9100010
+insert into role (title, salary, department_id) values'Compensation Analyst'4200011
+insert into role (title, salary, department_id) values'Human Resources Manager'7600011
+insert into role (title, salary, department_id) values'Occupational Therapist'8400011
+insert into role (title, salary, department_id) values'Payment Adjustment Coordinator'6900011
+insert into role (title, salary, department_id) values'Recruiter'6900011
+insert into role (title, salary, department_id) values'Sales Representative'8900011
+insert into role (title, salary, department_id) values'VP Sales'9500011
+insert into role (title, salary, department_id) values'Business Systems Development Analyst'5900012
---
+++ db/schema.sql
@@ -0,0 +1,33 @@
+DROP DATABASE IF EXIST company_db;
+CREATE DATABASE company_db;
+
+USE company_db;
+
+CREATE TABLE department(
+    id INT NOT NULL,
+    name VARCHAR(30),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+    id INT NOT NULL,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT,
+    PRIMARY KEY (id)
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+    ON DELETE SET NULL
+);
+
+CREATE TABLE employee (
+    id INT NOT NULL,
+    first_name VARCHAR(40),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT,
+    PRIMARY KEY (id)
+    FOREIGN KEY (role_id)
+    REFERENCES role(id)
+    ON DELETE SET NULL
+);
