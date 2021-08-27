5e7e3fe5d30e6908552cb55e411288cd986eca2d
minusInfinite <ashley@minusinfinite.net>
2021-08-27 17:48:27 +0930

    define database tables and seed data

--- db/schema.sql
+++ db/schema.sql
@@ -1,32 +1,32 @@
-DROP DATABASE IF EXIST company_db;
+DROP DATABASE IF EXISTS company_db;
 CREATE DATABASE company_db;
 
 USE company_db;
 
 CREATE TABLE department(
-    id INT NOT NULL,
+    id INT NOT NULL UNIQUE AUTO_INCREMENT,
     name VARCHAR(30),
     PRIMARY KEY (id)
 );
 
 CREATE TABLE role (
-    id INT NOT NULL,
-    title VARCHAR(30),
-    salary DECIMAL,
+    id INT NOT NULL UNIQUE AUTO_INCREMENT,
+    title VARCHAR(100),
+    salary DECIMAL(8,2),
     department_id INT,
-    PRIMARY KEY (id)
+    PRIMARY KEY (id),
     FOREIGN KEY (department_id)
     REFERENCES department(id)
     ON DELETE SET NULL
 );
 
 CREATE TABLE employee (
-    id INT NOT NULL,
+    id INT NOT NULL UNIQUE AUTO_INCREMENT,
     first_name VARCHAR(40),
     last_name VARCHAR(30),
     role_id INT,
-    manager_id INT,
-    PRIMARY KEY (id)
+    manager_id INT NULL,
+    PRIMARY KEY (id),
     FOREIGN KEY (role_id)
     REFERENCES role(id)
     ON DELETE SET NULL
